a496fac6854e923b16700a772ea170c8bb6637ab
dearseul <dearseulgi@gmail.com>
2023-03-23 20:50:45 +0900

    feat: -ing commit for merge

--- demo/src/main/java/todolist/model/Users.java
+++ demo/src/main/java/todolist/model/Users.java
@@ -1,21 +1,37 @@
 package todolist.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jdk.jshell.Snippet;
+import org.hibernate.annotations.CollectionId;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import todolist.model.commons.BaseEntity;
 
 import java.time.LocalDateTime;
 
 @Entity // todo Builder
-public class Users {
+public class Users extends BaseEntity {
     @Id @GeneratedValue
+    @Column
     private Long seq;
+    @Column
     private String userId;
+    @Column
     private String password;
+    @Column
     private String name;
-    private LocalDateTime createDt;
-    private LocalDateTime lastLoginDt;
+    @Column
     private String imgUrl;
+    @Column
     private int loginCnt;
+
+//    public static Snippet builder() {
+//    }
+
+    public void encodePassword(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(password);
+    }
 
 }
--- demo/src/main/java/todolist/repository/UserRepository.java
+++ demo/src/main/java/todolist/repository/UserRepository.java
@@ -2,13 +2,13 @@ package todolist.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import todolist.model.Users;
+
+import java.util.Optional;
 
 @Repository
-public class UserRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findById(String id);
 }
